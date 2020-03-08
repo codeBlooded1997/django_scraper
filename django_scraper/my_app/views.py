b34bef1d3dc8aadaba32d0b6f5811737e3baa229
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-03-08 00:50:43 -0500

    scraper functionality added

--- django_scraper/my_app/views.py
+++ django_scraper/my_app/views.py
@@ -1,8 +1,13 @@
-import requests
+from requests.compat import quote_plus 
 from bs4 import BeautifulSoup
 from django.shortcuts import render
+import requests
+
+from . import models
 
 
+
+BASE_CRAIGSLIST_URL = 'https://montreal.craigslist.org/search/sss?query={}&sort=rel&lang=en&cc=us'
 
 # Create your views here.
 def home(request):
@@ -11,6 +16,21 @@ def home(request):
 def new_search(request):
     # puling data out of search bar
     search = request.POST.get('search')
+    # Creating a search object from user's entry and storing in the admin's DB. SEXY!!!
+    models.Search.objects.create(search=search)
+    # Combining BASE_CRAIGSLIST_URL with input from user
+    # quote_plus -> Combines the user input into one string
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get('https://montreal.craigslist.org/search/bbb?query=tutor&sort=rel&lang=en&cc=us')
+    data = response.text
+    soup = BeautifulSoup(data)
+    result_listings = soup.findAll('li', class_='result-row')
+    post_titles = result_listings.findAll('a', class_='result-title hdrlnk')
+    post_date = result_listings.findAll('time',class_='result-date')
+    post_hood = result_listings.findAll('span', class_='result-hood')
+    print(post_titles[0].text)
+    print(post_date[0].text)
+    print(post_hood[0].text.strip())
     stuff_for_frontend = {
         'search': search,
     }
