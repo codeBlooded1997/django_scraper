b31dab6be1fd4a7866c752f101d6c870e5a463d6
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-03-08 13:46:05 -0400

    views.py UPDATED

--- django_scraper/my_app/views.py
+++ django_scraper/my_app/views.py
@@ -6,32 +6,48 @@ import requests
 from . import models
 
 
-
-BASE_CRAIGSLIST_URL = 'https://montreal.craigslist.org/search/sss?query={}&sort=rel&lang=en&cc=us'
+BASE_CRAIGSLIST_URL = 'https://montreal.craigslist.org/search/sss?query={}'
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
-    # puling data out of search bar
+    # pulling data out of search bar
     search = request.POST.get('search')
     # Creating a search object from user's entry and storing in the admin's DB. SEXY!!!
     models.Search.objects.create(search=search)
     # Combining BASE_CRAIGSLIST_URL with input from user
     # quote_plus -> Combines the user input into one string
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
-    response = requests.get('https://montreal.craigslist.org/search/bbb?query=tutor&sort=rel&lang=en&cc=us')
+    response = requests.get(final_url)
     data = response.text
-    soup = BeautifulSoup(data)
-    result_listings = soup.findAll('li', class_='result-row')
-    post_titles = result_listings.findAll('a', class_='result-title hdrlnk')
-    post_date = result_listings.findAll('time',class_='result-date')
-    post_hood = result_listings.findAll('span', class_='result-hood')
-    print(post_titles[0].text)
-    print(post_date[0].text)
-    print(post_hood[0].text.strip())
+    soup = BeautifulSoup(data, 'html.parser')
+    post_listings = soup.findAll('li', class_='result-row')
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find('a', class_='result-title hdrlnk').text
+        post_url = post.find('a', class_='result-title hdrlnk')['href']
+        post_date = post.find('time',class_='result-date').text
+        if post.find('span', class_='result-price'):
+            post_price = post.find('span', class_='result-price').text.strip()
+        else:
+            post_price = 'N/A'
+
+        if post.find('span', class_='result-hood'):
+            post_hood = post.find('span', class_='result-hood').text.strip()
+        else:
+            post_hood = 'N/A'
+
+        final_postings.append((post_date, post_title, post_hood, post_price, post_url))
+
+    quantity = len(final_postings)
+
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings,
+        'quantity': quantity,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
