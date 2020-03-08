2bddf7dd1b1aaafc7d20a243acf48768df18ba76
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-03-07 19:55:33 -0500

    Frontend CREATED
    frontend designed for both new_search.html and base.html .

--- django_scraper/my_app/views.py
+++ django_scraper/my_app/views.py
@@ -1,6 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render
+
 
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    # puling data out of search bar
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
